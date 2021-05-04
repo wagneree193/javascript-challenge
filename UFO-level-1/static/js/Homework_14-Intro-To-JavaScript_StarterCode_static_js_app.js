c6904d82be4586555084a2b6a10cd4ac01e9c4b5
wagneree193 <wagneree193@gmail.com>
2021-05-04 14:12:55 -0500

    started adding table and tweaked html

--- UFO-level-1/static/js/Homework_14-Intro-To-JavaScript_StarterCode_static_js_app.js
+++ UFO-level-1/static/js/Homework_14-Intro-To-JavaScript_StarterCode_static_js_app.js
@@ -1,4 +1,8 @@
 // from data.js
 var tableData = data;
+//  get a reference to the table and body
+var tbody = d3.select("tbody");
 
+// console.log the sighting data from the tableData variable
+console.log(tableData);
 // YOUR CODE HERE!
